b01339c93556b4bdcd81d817407d25355f0b06f7
leovoon <voonlihhaur@gmail.com>
2022-06-01 09:36:16 +0800

    ref: add prerender

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import CreateLink from '$lib/CreateLink.svelte';
 	import Footer from '$lib/Footer.svelte';
