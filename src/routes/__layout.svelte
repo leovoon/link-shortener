ba70b56dc02f897feb222d4437898bcfe3e6d213
leovoon <voonlihhaur@gmail.com>
2022-06-07 09:46:45 +0800

    move to use __layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Footer from '$lib/Footer.svelte';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="global.css" />
+</svelte:head>
+
+<header>
+	<h1>Pendek<sub>link-shortener</sub></h1>
+</header>
+<main>
+	<slot />
+</main>
+<Footer />
+
+<style>
+	h1 {
+		display: flex;
+		flex-direction: column;
+	}
+	sub {
+		font-size: clamp(1.4rem, 3.5vw, 2rem);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,5 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
-<script lang="ts">
+<script>
 	import CreateLink from '$lib/CreateLink.svelte';
-	import Footer from '$lib/Footer.svelte';
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="global.css" />
-</svelte:head>
-
-<header>
-	<h1>Pendek<sub>link-shortener</sub></h1>
-</header>
 <CreateLink />
-
-<Footer />
-
-<style>
-	h1 {
-		display: flex;
-		flex-direction: column;
-	}
-	sub {
-		font-size: clamp(1.4rem, 3.5vw, 2rem);
-	}
-</style>
