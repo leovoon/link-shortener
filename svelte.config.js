d86db8bdb00080897bbd1c42c3eeb1996f85f833
Leo Voon <voonlihhaur@gmail.com>
2023-07-27 12:40:24 +0800

    update: vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({ edge: true })
+		adapter: adapter({ runtime: 'edge' })
 	}
 };
 
