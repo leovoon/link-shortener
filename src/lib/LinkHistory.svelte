148acd01714ec2f4a2c28681eafa4672193e530b
Leo Voon <voonlihhaur@gmail.com>
2024-02-10 19:58:32 +0800

    add gridjs table

--- src/lib/LinkHistory.svelte
+++ src/lib/LinkHistory.svelte
@@ -1,71 +1,35 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { ShortLink } from './db/schema';
+	import { format } from 'timeago.js';
+	import Grid from 'gridjs-svelte';
+	import { h } from 'gridjs';
+
+	import 'gridjs/dist/theme/mermaid.css';
+
 	export let history: ShortLink[] = [];
+
+	const columns = [
+		{
+			name: 'Slug',
+			formatter: (cell: string) => h('a', { href: `${$page.url.hostname}/${cell}` }, cell)
+		},
+		'URL',
+		{
+			name: 'Created at',
+			formatter: (cell: string) => format(cell)
+		}
+	];
 </script>
 
 {#if history.length > 0}
-	<section>
-		<table class="styled-table">
-			<thead>
-				<tr>
-					<th>Slug</th>
-					<th>URL</th>
-					<th>Created At</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each history as item}
-					<tr>
-						<a href={`${$page.url.hostname}/${item.slug}`}>
-							<td>{`${$page.url}${item.slug}`}</td>
-						</a>
-						<td>{item.url}</td>
-						<td>{new Date(item.createdAt).toLocaleString()}</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</section>
+	<div class="container">
+		<Grid data={history} {columns} />
+	</div>
 {/if}
 
 <style>
-	section {
-		padding: 1rem;
-		overflow-x: auto;
-		display: flex;
-		justify-content: center;
-		margin-top: 1rem;
-	}
-	table {
-		--color-tone: #00987abd;
-		border-collapse: collapse;
-		font-size: 0.9em;
-		font-family: sans-serif;
-		border: 0.5px solid rgba(128, 128, 128, 0.288);
-	}
-
-	table thead tr {
-		background-color: var(--color-tone);
-		color: #ffffff;
-		text-align: left;
-	}
-
-	tr a {
-		color: #1b68c0;
-	}
-
-	table th,
-	table td {
-		padding: 12px 15px;
-	}
-
-	tbody tr td {
-		word-break: break-all;
-		word-wrap: break-word;
-	}
-
-	table tbody tr:nth-of-type(even) {
-		background-color: #d2fafdb6;
+	.container {
+		padding: clamp(0.5rem, 5vw, 5rem);
 	}
 </style>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import { superValidate, message, setError } from 'sveltekit-superforms';
 import { insertShortLinkSchema, shortlink } from '$lib/db/schema';
-import { fail } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
 import { db } from '$lib/db/client';
 import { eq } from 'drizzle-orm';
 import { zod } from 'sveltekit-superforms/adapters';
@@ -42,13 +42,13 @@ export const actions = {
 				.from(shortlink)
 				.where(eq(shortlink.id, Number(insertLink.insertId)));
 
-			if (returning.length === 0) setError(form, 'Failed to get created shortlink');
+			if (returning.length === 0) return setError(form, 'Failed to get created shortlink');
 
 			form.data = returning[0];
 			return message(form, 'Link created.');
 		} catch (e) {
 			console.error(e);
-			return fail(500, { form });
+			return error(500, { message: 'Server error. Please try again later.' });
 		}
 	}
 };
