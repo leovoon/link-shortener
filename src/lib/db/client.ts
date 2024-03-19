963181203db3a3ea65a4e8be68614867926b5ee8
Leo Voon <voonlihhaur@gmail.com>
2024-03-19 11:22:28 +0800

    fix middleware

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,8 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { DATABASE_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/neon-http';
-
+import { shortlink } from './schema';
 const sql = neon(DATABASE_URL);
 
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema: { shortlink } });
 console.log('DB initialized!')
